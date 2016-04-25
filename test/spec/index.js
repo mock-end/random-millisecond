53acba79d39791dbfc0db35d64244bf6fb040199
bubkoo <bubkoo.wy@gmail.com>
2016-04-25 22:28:50 +0800

    v1.0.2

--- test/spec/index.js
+++ test/spec/index.js
@@ -10,6 +10,8 @@ describe('random-millisecond: ', function () {
 
     while (count--) {
       expect(randomMillisecond()).to.be.within(0, 999);
+      expect(randomMillisecond({ min: 100 })).to.be.within(100, 999);
+      expect(randomMillisecond({ max: 100 })).to.be.within(0, 100);
     }
   });
 });
